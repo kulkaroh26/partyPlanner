0d0d464b50d6c854c7d376208d9d1d148da1f3ab
kulkaroh26 <114490265+kulkaroh26@users.noreply.github.com>
2025-02-07 10:58:04 -0500

    Create Partay.java
    Partay class for party planner

---
+++ Partay.java
@@ -0,0 +1,18 @@
+/**
+ * Partay.java Class for creating attendee objects in Party Planner
+ * @author Rohan Kulkarni
+ * @since 2/7/25
+ * 
+ * Preconditions: attendee objects, area layout (num seats, num tables)
+ * Postconditions: creates layout of party (num seats, num tables) as well as a list of methods used in tester class 
+*/
+import java.util.ArrayList; //import for Array Lists
+import java.util.Scanner;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+public class Partay {
+	
+	//constructors
+	public Partay
+	
+}
