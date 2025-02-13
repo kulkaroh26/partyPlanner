a09cdee876f9ce6b525c768e7baae13ce6887465
kulkaroh26 <114490265+kulkaroh26@users.noreply.github.com>
2025-02-13 09:30:15 -0500

    Update Partay.java
    This class is not done at all. I copied over the stuff from Tester class and converted it to method form. This includes creating the array list, adding people to them, manually registering people through inputs while checking if there is over 100 members. It does not yet have auto-sort functionality for tables and seats but I added the method for searching a person's given information about tables and seats with an input. Next step is to add guests to table and give them a seat number.

--- Partay.java
+++ Partay.java
@@ -12,7 +12,81 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 public class Partay {
 	
-	//constructors
-	public Partay
+	private int newTable;
+	private int newSeat;
+	private int numAttendees = 0;
+	private int searchNum;
 	
+	//constructors
+	public Partay (int table, int seat){
+		newTable = table;
+		newSeat = seat;
+		
+		ArrayList<Attendee> attList = new ArrayList<Attendee>();
+		try {
+			File myObj = new File("partyguests.txt");
+			Scanner myReader = new Scanner(myObj);
+			myReader.nextLine();
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				String[] dataSplit = data.split(",");
+				String tempName = dataSplit[2] + " " + dataSplit[1];
+				Attendee att = new Attendee(tempName, Integer.parseInt(dataSplit[3]));
+				attList.add(att);
+				numAttendees++;
+			
+			}
+			myReader.close();
+
+			} 
+		catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		} 
+	}
+	public void walkInRegister(){
+		if (int j=0;j<100-numAttendees;j++){
+			System.out.println("Would you like to register more attendees? y/n");
+			Scanner scan = new Scanner(System.in); //handles user inputs
+			String newReg = scan.nextLine();
+			String fName;
+			String lName;
+			if (newReg.equals("y")){
+				System.out.println("There are currently " + numAttendees + " registered");
+				if (numAttendees < 100)
+					System.out.println("Enter first name: ");
+					fName = scan.nextLine();
+					System.out.println("Enter last name: ");
+					lName = scan.nextLine();
+					System.out.println("Enter your company by ID: ");
+					int tempID = Integer.parseInt(scan.nextLine());
+					String tempName = fName + " " + lName;
+					Attendee att = new Attendee(tempName, tempID);
+					attList.add(att);
+					System.out.print("Name: " + attList.get(attList.size()-1).getName() + " ,Table ID: " + attList.get(attList.size()-1).getTableID()+ " ,Seat ID: " + attList.get(attList.size()-1).getSeatID() + " \n");
+					numAttendees++;
+				
+				}
+				else {
+					System.out.println("There are too many attendees, registration has ended");
+				}
+		
+			if (newReg.equals("n")){
+				System.out.println("You have chosen not to register");
+				break;
+			}
+		}
+		else {
+			return "Too many registers, cannot register further";
+		}
+	}
+	
+	public int getInfo(int searchCompNum){
+		searchNum = searchCompNum;
+		for (int i=0;i<attList.size()-1;i++){
+			if (attList.get(i).getCompanyID()==searchNum){
+				System.out.print("Name: " + attList.get(i).getName() + " ,Table ID: " + attList.get(i).getTableID()+ " ,Seat ID: " + attList.get(i).getSeatID() + " \n");
+			}
+		}
+	}	
 }
