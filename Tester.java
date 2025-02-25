858d7d2dd69a4bd427060eef0e048e1895654ed0
kulkaroh26 <114490265+kulkaroh26@users.noreply.github.com>
2025-02-24 22:34:06 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -4,74 +4,25 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 public class Tester {
 	public static void main (String[] args){
-		
-		/*int numAttendees = 0;
-		
-		ArrayList<Attendee> attList = new ArrayList<Attendee>();
-		try {
-			File myObj = new File("partyguests.txt");
-			Scanner myReader = new Scanner(myObj);
-			myReader.nextLine();
-			while (myReader.hasNextLine()) {
-				String data = myReader.nextLine();
-				String[] dataSplit = data.split(",");
-				String tempName = dataSplit[2] + " " + dataSplit[1];
-				Attendee att = new Attendee(tempName, Integer.parseInt(dataSplit[3]));
-				attList.add(att);
-				numAttendees++;
-				
-			}
-			myReader.close();
-
-			} 
-		catch (FileNotFoundException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		} 
-		
-		System.out.println("Would you like to register more attendees? y/n");
-		Scanner scan = new Scanner(System.in); //handles user inputs
-		String newReg = scan.nextLine();
-		if (newReg.equals("y"){
-			System.out.println("There are currently " + numAttendees + " registered");
-			if (numAttendees < 100){
-				for (int i=0;i<100-numAttendees;i++){
-					
-				}
-			}
-			else {
-				
-			}
-		}
-		if (newReg.equals("n"){
-			System.out.println("You have chosen not to register");
-		}
-		else {
-			System.out.print("Please enter a valid input");
-		}		
-		
-		
-		
-		Attendee a1 = new Attendee("Mr Posh", 12);
-		attList.add(a1);
-		Attendee a2 = new Attendee("Ms Posh", 13);
-		attList.add(a2);
-		Attendee a3 = new Attendee("Mr Nico", 14);
-		attList.add(a3);
-		System.out.println(attList.get(2).getCompanyID());
-		attList.get(2).setTableID(5);
-		System.out.println("Table ID: " + attList.get(2).getTableID());
-		
-		System.out.println("Search for Table ID: ");
-		
-		int searchNum = 13;
-		for (int i=0;i<attList.size()-1;i++){
-			if (attList.get(i).getCompanyID()==searchNum){
-				System.out.print("Name: " + attList.get(i).getName() + " ,Table ID: " + attList.get(i).getTableID()+ " ,Seat ID: " + attList.get(i).getSeatID());
-			}
-		}*/
-		
+		int x = 0;
+		int y = 0;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter the number of tables at the event: ");
+		x = scan.nextInt();
+		System.out.println("Please enter the number of seat at the event: ");
+		y = scan.nextInt();
 		Partay p1 = new Partay(10, 10);
-		
+		System.out.print("\n");
+		//while (true){
+			System.out.println("What would you like to do? \n 1. Register more attendees \n 2. Print rosters by table \n 3. Print roster by company \n 4. Find person's table and seat # \n 5. Quit program");
+			int option = scan.nextInt();
+			if (option==1){
+				p1.walkInRegister();
+			}
+			if (option==2){
+				System.out.print("What table do you want to print rosters for?");
+				
+			}
+		//}
 	}
 }
