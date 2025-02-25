af760a4a58bf655da6c4f9cdb0beaff8c029e447
kulkaroh26 <114490265+kulkaroh26@users.noreply.github.com>
2025-02-25 00:29:02 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -13,16 +13,25 @@ public class Tester {
 		y = scan.nextInt();
 		Partay p1 = new Partay(10, 10);
 		System.out.print("\n");
-		//while (true){
+		while (true){
 			System.out.println("What would you like to do? \n 1. Register more attendees \n 2. Print rosters by table \n 3. Print roster by company \n 4. Find person's table and seat # \n 5. Quit program");
 			int option = scan.nextInt();
 			if (option==1){
 				p1.walkInRegister();
 			}
 			if (option==2){
-				System.out.print("What table do you want to print rosters for?");
-				
+				System.out.println("What table do you want to print rosters for?");
+				int tableRost = scan.nextInt();
+				p1.getTableRoster(tableRost);
 			}
-		//}
+			if (option==4){
+				System.out.println("Enter the name of the person you want to search for: ");
+				String search = scan.nextLine();
+				p1.getInfo(search);
+			}
+			if (option==5){
+				break;
+			}
+		}
 	}
 }
